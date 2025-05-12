214a2fe5653c37c4602942bc655b19ef9a7a28bb
Medatarunvenkat <tarunvm0909@gmail.com>
2025-05-12 21:44:25 +0530

    Validation done

--- a1/script5.js
+++ a1/script5.js
@@ -2,5 +2,17 @@ function msg()
 {
     var email=document.getElementById('email').value;
     var pass=document.getElementById('pass').value;
-    document.getElementById('op').innerHTML="E-Mail is "+email+"<br> Password is :"+pass;
+    let output = document.getElementById('op');
+    let emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+            if (!emailPattern.test(email)) {
+                output.style.color = "red";
+                output.innerHTML = "Invalid email format.";
+            } else if (pass.length < 6) {
+                output.style.color = "red";
+                output.innerHTML = "Password must be at least 6 characters.";
+            } else {
+                output.style.color = "green";
+                output.innerHTML = "Login successful!<br>Email: " + email+"<br> Password is :"+pass;
+            }
 }
