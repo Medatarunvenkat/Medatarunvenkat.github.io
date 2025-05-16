13f5cd99ccfb92538f653170a1ce32210a93e025
Medatarunvenkat <tarunvm0909@gmail.com>
2025-05-16 19:35:25 +0530

    assign14

---
+++ a1/index14.js
@@ -0,0 +1,9 @@
+const prod=[
+    {id:1,"name":"prod1",price:100},
+    {id:2,"name":"prod2",price:200},
+    {id:3,"name":"prod3",price:300}
+]
+prod.forEach((x)=>{
+    x.price+=5;
+    console.log(x.name+" "+x.price);
+});
