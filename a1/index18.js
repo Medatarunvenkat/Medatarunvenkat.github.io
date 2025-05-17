441dd0f9cdf1803bc1127a39947824892106f221
Medatarunvenkat <tarunvm0909@gmail.com>
2025-05-17 20:47:51 +0530

    now

--- a1/index18.js
+++ a1/index18.js
@@ -2,14 +2,14 @@ function add(x,y)
 {
     return Promise.resolve(x+y)
 }
-async function sqr(a)
+function sqr(a)
 {
     return a*a
 }
 async function main()
 {
     let sum=await add(4,5)
-    let res=await sqr(sum)
+    let res=sqr(sum)
     console.log(res)
 }
 main()
