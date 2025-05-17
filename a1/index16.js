fa5c65db4c891c8dc56d395eb384a8e7f41b4d06
Medatarunvenkat <tarunvm0909@gmail.com>
2025-05-17 19:21:09 +0530

    done

--- a1/index16.js
+++ a1/index16.js
@@ -1,11 +1,23 @@
-const products=[
-    { id: 1, name: "product1", qty:3, price: 34, status:"pending"},
-    { id : 2, name: "product2", qty:5,  price: 56, status:"pending"},
-    { id : 3, name: "product2", qty:2, price: 43, status:"pending"},
+// const products=[
+//     { id: 1, name: "product1", qty:3, price: 34, status:"pending"},
+//     { id : 2, name: "product2", qty:5,  price: 56, status:"pending"},
+//     { id : 3, name: "product2", qty:2, price: 43, status:"pending"},
 
-];
+// ];
 //products.foreach((product) => console.log(product.name))
 
 
-const ins = products.map((product)=>({...product,price:product.price+5, status: "completed", total:product.price*product.qty})).filter(product=>product.qty>2);
-console.log(ins)
+// const ins = products.map((product)=>({...product,price:product.price+5, status: "completed", total:product.price*product.qty})).filter(product=>product.qty>2);
+// console.log(ins)
+
+const products=[
+    { id: 1, name: "product1",price: 34,qty:3},
+    { id : 2, name: "product2", price: 56,qty:5},
+    { id : 3, name: "product2",price: 43,qty:2},
+];
+
+const cart={1:7,3:5}
+products.map(value=>{
+    const qty=cart[value.id];
+    console.log(value.id,value.name,value.price,value.qty,value.price*qty);
+})
